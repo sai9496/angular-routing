47aedac7fe9c89648ad52e2a60468c70022ddaa6
gollapalliramesh <60209743+gollapalliramesh@users.noreply.github.com>
2020-01-24 12:13:13 +0530

    Added home state

--- js/all.js
+++ js/all.js
@@ -1,1 +1,10 @@
 var app = angular.module("myApp", ['ui.router']);
+app.config(function($stateProvider) {
+    var homeState = {
+        name: "home",
+        url: "/home",
+        templateUrl: "../home.html",
+        controller: "homeCtrl"
+    };
+    $stateProvider.state(homeState);
+});
